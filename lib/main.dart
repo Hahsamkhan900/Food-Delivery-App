9723e6759ba694a4a1659d447a3f025f80211b5f
Hasham1761999 <114433044+Hasham1761999@users.noreply.github.com>
2023-01-26 04:58:29 +0500

    Changes Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery/Screens/test.dart';
+import 'package:food_delivery/home/main_food_page.dart';
+//Home
 
 void main() {
-  runApp(const TestingPage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Colors.blue
+      ),
+      home: const MainFoodPage(),
+    );
+  }
 }
